f1e300f127a9bd4ffce93db25b936cd3254b36d1
hchung1 <hchung1@live.nmhu.edu>
2018-09-24 13:36:34 -0600

    done

--- src/sample_doctest.py
+++ src/sample_doctest.py
@@ -1,13 +1,13 @@
 # sample_doctest.py
 # Using unit tests included in docstrings 
-
+import doctest
 def pow_of_two(x):
     """Return x to the power of 2.
 
     >>> pow_of_two(4)
     16
     >>> pow_of_two(-2)
-    16
+    4
     """
     return x**2
 
@@ -23,15 +23,21 @@ def float_div_by_two(x):
 
 def get_evens(n):
     """Return the first n even numbers as a list.
+    >>> get_evens(10)
+    [0, 2, 4, 6, 8]
     """
-    return None
+    return range(0,n,2)
 
 def get_mid(data):
     """Return the midpoint value in data.
+    >>> get_mid([0, 1, 2, 3, 4, 5, 6])
+    3
     """
-    return None
+    return data[int(len(data)/2)]
 
 def get_last_half(data):
     """Return the values in the last half of data (slice) as a list.
+    >>> get_last_half([0, 1, 2, 3, 4, 5])
+    [3, 4, 5]
     """
-    return None
+    return data[int(len(data)/2)::]
